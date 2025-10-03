7339012908fe1fb606e12a137df3b83e212f3023
rpokidin <rpokidin@yandex.ru>
2025-10-03 16:13:23 +0300

    cmt 2-button

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.btn {
+    background: var(--btn-default);
+    display: inline-flex;
+    justify-content: center;
+    min-width: 335px;
+    font-size: 24px;
+    color: var(--white);
+    padding: 16px;
+    border-radius: 100px;
+}
+.btn:hover {
+    background: var(--btn-hover);
+}
+</style>
